a23b98a2a3c24b513871526189364b72cb4259c0
Dongle94 <sdy1029@gmail.com>
2023-04-04 09:47:59 +0900

    retinaface - utility copy trainset many sub dirs to calib dataset one directory

---
+++ utils/create_calib_set.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+
+cur_path = os.path.abspath(os.getcwd())
+trainset_path = os.path.join(cur_path, '../data/retinaface/train/images')
+calibset_path = os.path.join(cur_path, '../data/retinaface/calib/images')
+
+if not os.path.exists(calibset_path):
+    os.makedirs(calibset_path)
+
+for image_dir in os.listdir(trainset_path):
+    train_image_dir = os.path.join(trainset_path, image_dir)
+    for img in os.listdir(train_image_dir):
+        shutil.copyfile(os.path.join(train_image_dir, img), calibset_path + '/' + img)
