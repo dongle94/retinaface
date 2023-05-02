45bda7c4a084b7e59264aa2f7c670c256c400494
dongle94 <sdy1029@gmail.com>
2023-05-02 16:37:00 +0900

    refactor: test_widerface.py - get_boxes function refer roi['img_arr'] - add calculation mAP, mAR 0.5 to 0.95

--- test_widerface.py
+++ test_widerface.py
@@ -3,10 +3,8 @@ from __future__ import print_function
 import argparse
 import sys
 import os
-import time
+import datetime
 import numpy as np
-import mxnet as mx
-from mxnet import ndarray as nd
 import cv2
 from rcnn.logger import logger
 #from rcnn.config import config, default, generate_config
@@ -18,6 +16,9 @@ from rcnn.processing.nms import gpu_nms_wrapper
 from rcnn.processing.bbox_transform import bbox_overlaps
 from rcnn.dataset import retinaface
 from retinaface import RetinaFace
+
+
+os.environ['MXNET_CUDNN_AUTOTUNE_DEFAULT'] = '0'
 
 
 def parse_args():
@@ -85,14 +86,15 @@ imgid = -1
 
 def get_boxes(roi, pyramid):
     global imgid
-    im = cv2.imread(roi['image'])
+
+    im = roi['img_arr']
     do_flip = False
-    if not pyramid:
-        target_size = 1200
-        max_size = 1600
+    if not pyramid:     # args.mode=0
+        # target_size = 1200
+        # max_size = 1600
         #do_flip = True
-        target_size = 1504
-        max_size = 2000
+        # target_size = 1504
+        # max_size = 2000
         target_size = 1600
         max_size = 2150
         im_shape = im.shape
@@ -105,7 +107,6 @@ def get_boxes(roi, pyramid):
         scales = [im_scale]
     else:
         do_flip = True
-        #TEST_SCALES = [500, 800, 1200, 1600]
         TEST_SCALES = [500, 800, 1100, 1400, 1700]
         target_size = 800
         max_size = 1200
@@ -119,34 +120,38 @@ def get_boxes(roi, pyramid):
         scales = [
             float(scale) / target_size * im_scale for scale in TEST_SCALES
         ]
+
+    # do inference
     boxes, landmarks = detector.detect(im,
                                        threshold=args.thresh,
                                        scales=scales,
                                        do_flip=do_flip)
-    #print(boxes.shape, landmarks.shape)
+    # print(boxes.shape, landmarks.shape)
+
+    # if imgid < 0, save test image
     if imgid >= 0 and imgid < 100:
         font = cv2.FONT_HERSHEY_SIMPLEX
         for i in range(boxes.shape[0]):
             box = boxes[i]
-            ibox = box[0:4].copy().astype(np.int)
+            ibox = box[0:4].copy().astype(np.int32)
             cv2.rectangle(im, (ibox[0], ibox[1]), (ibox[2], ibox[3]),
                           (255, 0, 0), 2)
-            #print('box', ibox)
-            #if len(ibox)>5:
+            print('box', ibox)
+            # if len(ibox)>5:
             #  for l in range(5):
             #    pp = (ibox[5+l*2], ibox[6+l*2])
             #    cv2.circle(im, (pp[0], pp[1]), 1, (0, 0, 255), 1)
-            blur = box[5]
-            k = "%.3f" % blur
-            cv2.putText(im, k, (ibox[0] + 2, ibox[1] + 14), font, 0.6,
-                        (0, 255, 0), 2)
-            #landmarks = box[6:21].reshape( (5,3) )
+            # blur = box[4]
+            # k = "%.3f" % blur
+            # cv2.putText(im, k, (ibox[0] + 2, ibox[1] + 14), font, 0.6,
+            #             (0, 255, 0), 2)
+            # landmarks = box[6:21].reshape( (5,3) )
             if landmarks is not None:
                 for l in range(5):
                     color = (0, 255, 0)
                     landmark = landmarks[i][l]
                     pp = (int(landmark[0]), int(landmark[1]))
-                    if landmark[2] - 0.5 < 0.0:
+                    if landmark[1] - 0.5 < 0.0:
                         color = (0, 0, 255)
                     cv2.circle(im, (pp[0], pp[1]), 1, color, 2)
         filename = './testimages/%d.jpg' % imgid
@@ -158,86 +163,91 @@ def get_boxes(roi, pyramid):
 
 
 def test(args):
-    print('test with', args)
+    print(f'** test with args: {args}')
+
+    # Initialize
     global detector
     output_folder = args.output
     if not os.path.exists(output_folder):
         os.mkdir(output_folder)
-    detector = RetinaFace(args.prefix,
-                          args.epoch,
-                          args.gpu,
+    # Get Network
+    detector = RetinaFace(args.prefix, args.epoch, args.gpu,
                           network=args.network,
                           nocrop=args.nocrop,
                           vote=args.bbox_vote)
-    imdb = eval(args.dataset)(args.image_set, args.root_path,
-                              args.dataset_path)
-    roidb = imdb.gt_roidb()
-    gt_overlaps = np.zeros(0)
-    overall = [0.0, 0.0]
-    gt_max = np.array((0.0, 0.0))
-    num_pos = 0
-    print('roidb size', len(roidb))
 
+    # Create Dataset class
+    imdb = eval(args.dataset)(args.image_set, args.root_path, args.dataset_path)
+    roidb = imdb.gt_roidb()
+
+    overall = [0.0, 0.0, 0]
+    overall_05 = [0.0, 0.0, 0]
+
+    num_pos = 0
+    print(f'** roidb size: {len(roidb)}')
+
+    # Metric
+    iouv = np.linspace(0.5, 0.95, 10)
+    output_file = os.path.join(output_folder,
+                               (datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")) + '.txt')
     for i in range(len(roidb)):
         if i % args.parts != args.part:
             continue
-        #if i%10==0:
-        #  print('processing', i, file=sys.stderr)
+
         roi = roidb[i]
         boxes = get_boxes(roi, args.pyramid)
-        if 'boxes' in roi:
+        if 'boxes' in roi and roi['boxes'].size > 0 and boxes.size > 0:
             gt_boxes = roi['boxes'].copy()
-            gt_areas = (gt_boxes[:, 2] - gt_boxes[:, 0] +
-                        1) * (gt_boxes[:, 3] - gt_boxes[:, 1] + 1)
             num_pos += gt_boxes.shape[0]
 
-            overlaps = bbox_overlaps(boxes.astype(np.float),
-                                     gt_boxes.astype(np.float))
-            #print(im_info, gt_boxes.shape, boxes.shape, overlaps.shape, file=sys.stderr)
-
+            overlaps = bbox_overlaps(boxes.astype(np.float), gt_boxes.astype(np.float))
+            # print(gt_boxes.shape, boxes.shape, overlaps.shape, file=sys.stderr)
             _gt_overlaps = np.zeros((gt_boxes.shape[0]))
 
-            if boxes.shape[0] > 0:
-                _gt_overlaps = overlaps.max(axis=0)
-                #print('max_overlaps', _gt_overlaps, file=sys.stderr)
-                for j in range(len(_gt_overlaps)):
-                    if _gt_overlaps[j] > 0.5:
-                        continue
-                    #print(j, 'failed', gt_boxes[j],  'max_overlap:', _gt_overlaps[j], file=sys.stderr)
+            pred_overlaps = overlaps.max(axis=1)
+            true_overlaps = overlaps.max(axis=0)
 
-                # append recorded IoU coverage level
-                found = (_gt_overlaps > 0.5).sum()
-                recall = found / float(gt_boxes.shape[0])
-                #print('recall', _recall, gt_boxes.shape[0], boxes.shape[0], gt_areas, 'num:', i, file=sys.stderr)
-                overall[0] += found
-                overall[1] += gt_boxes.shape[0]
-                #gt_overlaps = np.hstack((gt_overlaps, _gt_overlaps))
-                #_recall = (gt_overlaps >= threshold).sum() / float(num_pos)
-                recall_all = float(overall[0]) / overall[1]
-                #print('recall_all', _recall, file=sys.stderr)
-                print('[%d]' % i,
-                      'recall',
-                      recall, (gt_boxes.shape[0], boxes.shape[0]),
-                      'all:',
-                      recall_all,
-                      file=sys.stderr)
+            if boxes.shape[0] > 0:
+                with open(output_file, 'a') as f:
+                    for iou in iouv:
+                        ap_tp = (pred_overlaps > iou).sum()
+                        ap_fp = len(pred_overlaps) - ap_tp
+
+                        ar_tp = (true_overlaps > iou).sum()
+                        ar_fn = len(true_overlaps) - ar_tp
+
+                        precision = ap_tp / len(pred_overlaps)
+                        recall = ar_tp / len(true_overlaps)
+
+                        if iou == 0.5:
+                            overall_05[0] += precision
+                            overall_05[1] += recall
+                            overall_05[2] += 1
+
+                            map50 = overall_05[0] / overall_05[2]
+                            mar50 = overall_05[1] / overall_05[2]
+
+                            name = '/'.join(roidb[i]['image'].split('/')[-2:])
+                            f.write(f"[{i}] {name} ")
+                            f.write(
+                                f"precision: {precision:.4f} / recall: {recall:.4f} / mAP 0.5: {map50:.4f} / mAR 0.5: {mar50:.4f}\n")
+
+                        overall[0] += precision
+                        overall[1] += recall
+                        overall[2] += 1
+
+                    map5095 = overall[0] / overall[2]
+                    mar5095 = overall[1] / overall[2]
+                    print(f"{[i]} mAP 0.5: {map50:.4f} / mAR 0.5: {mar50:.4f} / mAP 0.5:0.95: {map5095:.4f} / mAR 0.5:0.95: {mar5095:.4f}",
+                          file=sys.stderr)
+
+                    # Write inference result to txt file
+                    if i % 10 == 0:
+                        f.write(
+                            f"{[i]} mAP 0.5: {map50:.4f} / mAR 0.5: {mar50:.4f} / mAP 0.5:0.95: {map5095:.4f} / mAR 0.5:0.95: {mar5095:.4f}\n")
+
         else:
             print('[%d]' % i, 'detect %d faces' % boxes.shape[0])
-
-        _vec = roidb[i]['image'].split('/')
-        out_dir = os.path.join(output_folder, _vec[-2])
-        if not os.path.exists(out_dir):
-            os.mkdir(out_dir)
-        out_file = os.path.join(out_dir, _vec[-1].replace('jpg', 'txt'))
-        with open(out_file, 'w') as f:
-            name = '/'.join(roidb[i]['image'].split('/')[-2:])
-            f.write("%s\n" % (name))
-            f.write("%d\n" % (boxes.shape[0]))
-            for b in range(boxes.shape[0]):
-                box = boxes[b]
-                f.write(
-                    "%d %d %d %d %g \n" %
-                    (box[0], box[1], box[2] - box[0], box[3] - box[1], box[4]))
 
 
 def main():
@@ -251,7 +261,7 @@ def main():
     elif args.mode == 2:
         args.pyramid = True
         args.bbox_vote = False
-    logger.info('Called with argument: %s' % args)
+    logger.info(' ** Called with argument: %s' % args)
     test(args)
 
 
